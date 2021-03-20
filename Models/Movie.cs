66b08d34148c2d9ea546b34cb8cff6e9c4f8e217
TimLiu2019 <qitingliu7@gmail.com>
2021-03-20 18:49:53 -0400

    add movie model and MovieContext

---
+++ Models/Movie.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Movie_API_DotNetCore.Models
+{
+    public class Movie
+    {
+        [Required]
+        [Key]
+        public Guid Id { get; set; }
+
+        [Required]
+        public string Title  { get; set; }
+
+        [Required]
+        public int Rate { get; set; }
+
+        [Required]
+        public string Genre { get; set; }
+
+        [Required]
+        public int Stock { get; set; }
+
+       
+        public Boolean Like { get; set; }
+
+    }
+}
