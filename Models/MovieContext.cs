de3ba4ad7bd7b261e8de0bb3bb0f2bb56133a5d5
TimLiu2019 <qitingliu7@gmail.com>
2021-03-20 19:59:31 -0400

    add movie api

--- Models/MovieContext.cs
+++ Models/MovieContext.cs
@@ -2,10 +2,17 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
 
 namespace Movie_API_DotNetCore.Models
 {
-    public class MovieContext
+    public class MovieContext: DbContext
     {
+        public MovieContext(DbContextOptions<MovieContext> options)
+           : base(options)
+        {
+        }
+
+        public DbSet<Movie> Movies { get; set; }
     }
 }
